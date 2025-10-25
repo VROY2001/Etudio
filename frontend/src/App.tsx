18b236a981561adb2bc65ac3573dc669c4bee5c2
Vagdhan Roy <vagdhanroy@gmail.com>
2025-10-25 21:52:20 +1100

    Fixing piano component and bg

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import Piano from './components/Piano';
+import React from "react";
+import Piano from "./components/Piano";
+import brownPaper from "./assets/brown-paper.jpg";
 
 function App() {
   return (
-    <div>
-      <h1>Etudio - Chopin AI Composer</h1>
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#d2b48c",
+        backgroundImage: `url(${brownPaper})`,
+        backgroundRepeat: "repeat",
+        backgroundSize: "auto",
+        backgroundPosition: "center",
+      }}
+    >
+      <h1 style={{ textAlign: "center", marginBottom: "40px" }}>
+        Etudio - Chopin AI Composer
+      </h1>
       <Piano />
     </div>
   );
